b93274caca683a30fbb1c761a7f782c566223476
Bartosz Meglicki <bmegli@op.pl>
2016-11-14 13:45:48 +0100

    let laser motor spin for 2 sec before sync

--- ev3laser/main.cpp
+++ ev3laser/main.cpp
@@ -93,7 +93,7 @@ int main(int argc, char **argv)
 	InitNetworkUDP(&socket_udp, &address_udp, host, port, 0);
 	InitLaserMotor(&motor, duty_cycle);
 	//let the motor spin for a while
-	Sleep(1000); 
+	Sleep(2000); 
 	 
  	if( InitLaser(&laser, laser_tty, LASER_FRAMES_PER_READ, crc_tolerance_pct) !=SUCCESS )
 	{
