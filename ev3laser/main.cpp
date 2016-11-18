dd0fedbe88c8575210fd3ebf4baa73de79d2df12
Bartosz Meglicki <bmegli@op.pl>
2016-11-18 14:47:52 +0100

    realtime correction for lidar timestaping (#26)
    addresses #12

--- ev3laser/main.cpp
+++ ev3laser/main.cpp
@@ -42,18 +42,21 @@ volatile sig_atomic_t g_finish_program=0;
 
 const int TTY_PATH_MAX=100;
 const int LASER_FRAMES_PER_READ=10;
+const int LASER_FRAMES_PER_ROTATION=90;
+const uint64_t MICROSECONDS_PER_MINUTE=60000000;
+const uint64_t LASER_SPEED_FIXED_POINT_PRECISION=64;
 
 struct laser_packet
 {
 	uint64_t timestamp_us;
 	uint16_t laser_speed; //fixed point, 6 bits precision, divide by 64.0 to get floating point 
 	uint16_t laser_angle; //angle of laser_readings[0]
-	laser_reading laser_readings[4*LASER_FRAMES_PER_READ];
+	xv11lidar_reading laser_readings[4*LASER_FRAMES_PER_READ];
 };
 
 const int LASER_PACKET_BYTES = 12 + 16 * LASER_FRAMES_PER_READ;
 
-void MainLoop(int socket_udp, const struct sockaddr_in &address, struct xv11lidar_data *laser, ev3dev::dc_motor *laser_motor);
+void MainLoop(int socket_udp, const struct sockaddr_in &address, struct xv11lidar *laser, ev3dev::dc_motor *laser_motor);
 
 int ProcessInput(int argc, char **argv, int *port, int *duty_cycle, int *crc_tolerance_pct);
 void Usage();
@@ -62,8 +65,8 @@ void Finish(int signal);
 
 void InitLaserMotor(ev3dev::dc_motor *m, int duty_cycle);
 
-int EncodeLaserReading(const laser_reading *reading, char *data);
-int EncodeLaserFrame(const laser_frame *frame, char *data);
+int EncodeLaserReading(const xv11lidar_reading *reading, char *data);
+int EncodeLaserFrame(const xv11lidar_frame *frame, char *data);
 int EncodeLaserPacket(const laser_packet &p, char *data);
 
 void SendLaserPacket(int socket_udp, const sockaddr_in &dst, const laser_packet &packet);
@@ -72,9 +75,8 @@ int main(int argc, char **argv)
 {
 	int socket_udp;
 	struct sockaddr_in address_udp;
-	struct xv11lidar_data laser;    
-	int port;
-	int duty_cycle, crc_tolerance_pct;
+	struct xv11lidar *laser;    
+	int port, duty_cycle, crc_tolerance_pct;
 	
 	if( ProcessInput(argc, argv, &port, &duty_cycle, &crc_tolerance_pct) )
 	{
@@ -92,18 +94,16 @@ int main(int argc, char **argv)
 	RegisterSignals(Finish);
 	InitNetworkUDP(&socket_udp, &address_udp, host, port, 0);
 	InitLaserMotor(&motor, duty_cycle);
-	//let the motor spin for a while
-	Sleep(2000); 
 	 
- 	if( InitLaser(&laser, laser_tty, LASER_FRAMES_PER_READ, crc_tolerance_pct) !=SUCCESS )
+ 	if( (laser=xv11lidar_init(laser_tty, LASER_FRAMES_PER_READ, crc_tolerance_pct)) == NULL )
 	{
 		fprintf(stderr, "ev3laser: init laser failed\n");
 		g_finish_program=true;
 	}
 
-	MainLoop(socket_udp, address_udp, &laser, &motor);
+	MainLoop(socket_udp, address_udp, laser, &motor);
 
-	CloseLaser(&laser);
+	xv11lidar_close(laser);
 	motor.stop();
 	CloseNetworkUDP(socket_udp);
 
@@ -112,40 +112,60 @@ int main(int argc, char **argv)
 	return 0;	
 }
 
-void MainLoop(int socket_udp, const struct sockaddr_in &address, struct xv11lidar_data *laser, ev3dev::dc_motor *laser_motor)
+void MainLoop(int socket_udp, const struct sockaddr_in &address, struct xv11lidar *laser, ev3dev::dc_motor *laser_motor)
 {
 	struct laser_packet packet;
-	struct laser_frame frames[LASER_FRAMES_PER_READ];
-	uint64_t last_timestamp;
-	uint32_t rpm;
+	struct xv11lidar_frame frames[LASER_FRAMES_PER_READ];
+	uint64_t timestamp_reference, timestamp_measured, timespan_computed, correction, total_correction=0, max_correction=0;
+	uint32_t rpm, sane_frames;
 	int status, counter, benchs=INT_MAX;
-
+	
 	uint64_t start=TimestampUs();	
-	last_timestamp=start;
+	timestamp_reference=start;
 		
 	for(counter=0;!g_finish_program && counter<benchs;++counter)
 	{
-		packet.timestamp_us=last_timestamp;
+		packet.timestamp_us=timestamp_reference;
 		
-		if( (status=ReadLaser(laser, frames)) != SUCCESS )
+		if( (status=xv11lidar_read(laser, frames)) != XV11LIDAR_SUCCESS )
 		{
 			fprintf(stderr, "ev3laser: ReadLaser failed with status %d\n", status);
 			break;
 		}
 		// when read is finished, next read proceeds
-		last_timestamp=TimestampUs(); 
-
+		timestamp_measured=TimestampUs(); 
+		
 		packet.laser_angle=(frames[0].index-0xA0)*4;
 		
-		rpm=0;
+		rpm=sane_frames=0;
+	
 		for(int i=0;i<LASER_FRAMES_PER_READ;++i)
 		{
-			rpm+=frames[i].speed;
-			memcpy(packet.laser_readings+4*i, frames[i].readings, 4*sizeof(laser_reading));
+			memcpy(packet.laser_readings+4*i, frames[i].readings, 4*sizeof(xv11lidar_reading));
+			if(frames[i].readings[0].invalid_data == 0 || frames[i].readings[0].distance != XV11LIDAR_CRC_FAILURE)
+			{
+				++sane_frames;
+				rpm+=frames[i].speed;
+			}
+
 		}
 		
-		packet.laser_speed=rpm/LASER_FRAMES_PER_READ;
-		
+		packet.laser_speed=rpm/sane_frames;
+		timespan_computed = MICROSECONDS_PER_MINUTE * LASER_FRAMES_PER_READ * LASER_SPEED_FIXED_POINT_PRECISION / (LASER_FRAMES_PER_ROTATION * packet.laser_speed);
+		if(timestamp_measured - timespan_computed < timestamp_reference)
+		{ // new timestamp has better value, use it from now on
+			timestamp_reference = timestamp_measured;
+			packet.timestamp_us = timestamp_measured - timespan_computed;
+		}
+		else
+		{ // reference timetamp is better, correct measured timestamp from reference timestamp
+			timestamp_reference += timespan_computed;
+			correction = timestamp_measured - timestamp_reference;
+			if(correction > max_correction)
+				max_correction= correction;
+			total_correction += correction;
+		}
+
 		SendLaserPacket(socket_udp, address, packet);
 		
 		if(IsStandardInputEOF()) //the parent process has closed it's pipe end
@@ -154,8 +174,11 @@ void MainLoop(int socket_udp, const struct sockaddr_in &address, struct xv11lida
 	
 	uint64_t end=TimestampUs();
 	double seconds_elapsed=(end-start)/ 1000000.0L;
-	printf("ev3laser: average loop %f seconds\n", seconds_elapsed/counter);
+	
+	printf("ev3laser: avg loop %f seconds\n", seconds_elapsed/counter);
 	printf("ev3laser: last laser speed %f\n", packet.laser_speed/64.0);
+	printf("ev3laser: max timestamp correction %llu\n", max_correction);
+	printf("ev3laser: avg timestamp correction %llu\n", total_correction/counter);
 }
 
 
@@ -216,7 +239,7 @@ void InitLaserMotor(ev3dev::dc_motor *m, int duty_cycle)
 }
 
  
-int EncodeLaserReading(const laser_reading *reading, char *data)
+int EncodeLaserReading(const xv11lidar_reading *reading, char *data)
 {
 	const uint16_t *reading_as_u16=(uint16_t*)reading;
 	
@@ -228,7 +251,7 @@ int EncodeLaserReading(const laser_reading *reading, char *data)
 	return 4;
 }
 
-int EncodeLaserFrame(const laser_frame *frame, char *data)
+int EncodeLaserFrame(const xv11lidar_frame *frame, char *data)
 {
 	*data=frame->start;
 	++data;
@@ -268,7 +291,6 @@ int EncodeLaserPacket(const laser_packet &p, char *data)
 void SendLaserPacket(int socket_udp, const sockaddr_in &dst, const laser_packet &packet)
 {
 	static char buffer[LASER_PACKET_BYTES];
-	//memcpy(buffer, &packet, sizeof(laser_packet));
 	EncodeLaserPacket(packet, buffer);
 	SendToUDP(socket_udp, dst, buffer, LASER_PACKET_BYTES);
 }
