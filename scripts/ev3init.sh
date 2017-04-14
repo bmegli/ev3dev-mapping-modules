57be13e9e8e998d5459b9738a5ac8da3a436b528
bmegli <bmegli@op.pl>
2017-04-14 09:52:33 +0000

    warmup LIDAR motors in init script
    informs user during initialization script, added script comments

--- scripts/ev3init.sh
+++ scripts/ev3init.sh
@@ -1,16 +1,52 @@
 #!/usr/bin/env bash
 
-# Load the MicroInfnity CruizCore XG1300L gyroscope I2C driver manually, it can't be autodetected
+# This script expects:
+# -CruizCore XG1300L gyroscope (input 3)
+# -2 XV11 LIDARS (input 1 and 2, and outputs B and C)
+# -2 Large Servo Motors (outputs A and D)
+#
+# Script:
+# - loads CruizCore XG1300L driver manually (it can't be autodetected)
+# - changes inputs 1 and 2 modes to other-uart for XV11-LIDAR uart communication
+# - changes outputs B and C modes to dc-motor for XV11-LIDAR motors control
+# - spins the motors for some time to warm-up the LIDARS (they tend to work unstable when cold starting)
+
+echo 'ev3dev-mapping-modules initialization script'
+echo '(once after boot to init ports for lidars and gyroscope)'
+echo ''
+
+echo 'Loading MicroInfnity CruizCore XG1300L gyroscope I2C driver'
 echo mi-xg1300l 0x01 > /sys/bus/i2c/devices/i2c-5/new_device
 
-# Change input 1 mode to other-uart, for XV11-LIDAR uart communication
+echo 'Changing input 1 mode to other-uart (XV11-LIDAR 1)'
 echo other-uart > /sys/class/lego-port/port0/mode
-
-# Same for input 2 (second lidar)
+echo 'Changing input 2 mode to other-uart (XV11-LIDAR 2)'
 echo other-uart > /sys/class/lego-port/port1/mode
 
-# Change output port B mode to dc-motor, this is for XV11-LIDAR motor control
+echo 'Changing output B mode to dc-motor (XV11-LIDAR 1 motor)'
 echo dc-motor > /sys/class/lego-port/port5/mode
-
-# Same for output port C (second lidar)cd 
+echo 'Changing output C mode to dc-motor (XV11-LIDAR 2 motor)'
 echo dc-motor > /sys/class/lego-port/port6/mode
+
+echo 'Waiting for dc-motor devices to be created'
+sleep 1
+
+echo 'Setting duty cycle setpoint for XV11-LIDAR 1 motor'
+echo 44 > /sys/class/dc-motor/motor0/duty_cycle_sp
+echo 'Setting duty cycle setpoint for XV11-LIDAR 2 motor'
+echo 44 > /sys/class/dc-motor/motor1/duty_cycle_sp
+
+echo 'Warming up the XV11-LIDAR 1 motor (spinning 20 seconds)'
+echo run-direct > /sys/class/dc-motor/motor0/command
+echo 'Warming up the XV11-LIDAR 2 motor (spinning 20 seconds)'
+echo run-direct > /sys/class/dc-motor/motor1/command
+sleep 20
+
+echo 'Stopping XV11-LIDAR 1 motor'
+echo stop > /sys/class/dc-motor/motor0/command
+echo 'Stopping XV11-LIDAR 2 motor'
+echo stop > /sys/class/dc-motor/motor1/command
+
+echo 'Done'
+
+
