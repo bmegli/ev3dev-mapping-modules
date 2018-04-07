7b69ef79097062dc452b8ba85ffcaba8ae826126
Bartosz Meglicki <bmegli@op.pl>
2018-04-07 18:45:28 +0000

    configure pins for /dev/ttyO2 in init script
    This is for getting data from the second LIDAR

--- scripts/ccinit.sh
+++ scripts/ccinit.sh
@@ -4,6 +4,10 @@ echo 'configuring pins for UART 1'
 config-pin p9.24 uart
 config-pin p9.26 uart
 
+echo 'configuring pins for UART 2'
+config-pin p9.21 uart
+config-pin p9.22 uart
+
 echo 'configuring pins for UART 4'
 config-pin p9.11 uart
 config-pin p9.13 uart
